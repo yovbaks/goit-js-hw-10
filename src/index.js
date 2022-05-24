67b4321b7bd6e8241c60bf8a8ad56a27433a8c0c
yovbaks <yovbak.s@gmail.com>
2022-05-24 23:05:07 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import fetchCountry from './js/fetchCountries'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import countryListTemplate from './templates/countryList.hbs'
+import countryInfoTemplate from './templates/countryInfo.hbs'
 
 const DEBOUNCE_DELAY = 300;
+
+// const refs = {
+//     inputCountrySearch: document.querySelector('#search-box'),
+//     countryList: document.querySelector('.countryList'),
+//     countryInfo: document.querySelector('.country-info'),
+// }
+const inputCountrySearchRef = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
+
+
+inputCountrySearchRef.addEventListener('input', debounce(onFetchCountry, DEBOUNCE_DELAY));
+
+function onFetchCountry(event) {
+    event.preventDefault();
+
+    const inputValue = event.target.value.trim();
+    console.log(inputValue);
+
+    countryListRef.innerHTML = "";
+    countryInfoRef.innerHTML = '';
+    
+
+    fetchCountry(inputValue).then(response => {
+        console.log(response);
+        console.log(response.length);
+        if (response.length > 10) {
+            Notify.info('Too many matches found. Please enter a more specific name.')
+        } else if (response.length >= 2 && response.length <= 10) {
+            // console.log(response);
+            // countryListRef.innerHTML =`Hello`
+            renderCountryList(response);
+        } else if (response.length === 1) {
+            renderCountryInfo(response);
+        }
+        // renderCountryList(response);
+    }).catch(error=> Notify.failure('Хуйня якась'))
+}
+
+function renderCountryList(country) {
+    countryListRef.innerHTML = countryListTemplate(country);
+}
+
+function renderCountryInfo(country) {
+    countryInfoRef.innerHTML = countryInfoTemplate(country);
+}
