a8d42383876be8390aa54c8e01091703521c42f9
yovbaks <yovbak.s@gmail.com>
2022-05-25 12:06:20 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import fetchCountry from './js/fetchCountries'
+import fetchCountry from './js/fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import countryListTpl from './templates/countryList.hbs'
-import countryInfoTpl from './templates/countryInfo.hbs'
+import countryListTpl from './templates/countryList.hbs';
+import countryInfoTpl from './templates/countryInfo.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -16,39 +16,39 @@ const inputCountrySearchRef = document.querySelector('#search-box');
 const countryListRef = document.querySelector('.country-list');
 const countryInfoRef = document.querySelector('.country-info');
 
-
 inputCountrySearchRef.addEventListener('input', debounce(onFetchCountry, DEBOUNCE_DELAY));
 
 function onFetchCountry(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const inputValue = event.target.value.trim();
-    console.log(inputValue);
+  const inputValue = event.target.value.trim();
+  console.log(inputValue);
 
-    countryListRef.innerHTML = "";
-    countryInfoRef.innerHTML = '';
-    
+  countryListRef.innerHTML = '';
+  countryInfoRef.innerHTML = '';
 
-    fetchCountry(inputValue).then(response => {
-        console.log(response);
-        console.log(response.length);
-        if (response.length > 10) {
-            Notify.info('Too many matches found. Please enter a more specific name.')
-        } else if (response.length >= 2 && response.length <= 10) {
-            // console.log(response);
-            // countryListRef.innerHTML =`Hello`
-            renderCountryList(response);
-        } else if (response.length === 1) {
-            renderCountryInfo(response);
-        }
-        // renderCountryList(response);
-    }).catch(error=> Notify.failure('Oops, there is no country with that name'))
+  fetchCountry(inputValue)
+    .then(response => {
+      console.log(response);
+      console.log(response.length);
+      if (response.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+      } else if (response.length >= 2 && response.length <= 10) {
+        // console.log(response);
+        // countryListRef.innerHTML =`Hello`
+        renderCountryList(response);
+      } else if (response.length === 1) {
+        renderCountryInfo(response);
+      }
+      // renderCountryList(response);
+    })
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
 }
 
 function renderCountryList(country) {
-    countryListRef.innerHTML = countryListTpl(country);
+  countryListRef.innerHTML = countryListTpl(country);
 }
 
 function renderCountryInfo(country) {
-    countryInfoRef.innerHTML = countryInfoTpl(country);
+  countryInfoRef.innerHTML = countryInfoTpl(country);
 }
